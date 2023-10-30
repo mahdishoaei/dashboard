6d93eba3bb8260ed20d2bd4681da3ac73344616b
mahdi shoaei <mahdishoaei98@gmail.com>
2023-10-30 18:50:42 +0330

    add validation to login page

--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -1,20 +1,81 @@
+<script setup>
+
+definePageMeta({
+    layout: "auth",
+});
+
+import { validEmail,validPassword } from '@/utils/validate'
+
+const form = ref({
+    email : '',
+    password : ''
+})
+const error = ref({
+    email : {
+        status : false,
+        message : ''
+    },
+    password : {
+        status : false,
+        message : ''
+    }
+})
+
+const handleLogin = ()=>{
+    let AccessToLogin = true
+    if(!validEmail(form.value.email)){
+        AccessToLogin = false
+        error.value.email.status = true
+        error.value.email.message = 'invalid email !!!'
+    } else {
+        AccessToLogin = true
+        error.value.email.status = false
+        error.value.email.message = ''
+    }
+    if(!validPassword(form.value.password)){
+        AccessToLogin = false
+        error.value.password.status = true
+        error.value.password.message = 'Password  must be more than 8 charecter'
+    } else {
+        AccessToLogin = true
+        error.value.password.status = false
+        error.value.password.message = ''
+    }
+}
+
+</script>
+
 <template>
     <div class="login-container">
     <div class="form-container d-flex flex-column px-6 py-8">
       <span class="app-font-size-16 app-font-weight-500 app-color-white"> Sign in</span>
-      <CoreInput class="mt-6" label="Email"/>
-      <CoreInput class="mt-2" label="Password"/>
-      <CoreBtn class="mt-4" background="primary" name="submit" width="85px" height="30px" borderRadius="5px"/>
+      <CoreInput 
+      class="mt-6" 
+      label="Email"
+      v-model="form.email"
+      :error="error.email.status"
+      :messageError="error.email.message"
+      />
+      <CoreInput 
+      class="mt-2" 
+      label="Password"
+      v-model="form.password"
+      :error="error.password.status"
+      :messageError="error.password.message"
+      />
+      <CoreBtn 
+      class="mt-4" 
+      background="primary"
+      name="submit" 
+      width="85px" 
+      height="30px" 
+      borderRadius="5px"
+      @click="handleLogin"
+      />
     </div>
     </div>
 </template>
 
-
-<script setup>
-definePageMeta({
-    layout: "auth",
-});
-</script>
 
 <style scoped>
 .login-container{
