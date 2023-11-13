6e64c3ef69fce818b63a3396c02fce500cdc5a13
mahdi shoaei <mahdishoaei98@gmail.com>
2023-11-13 19:16:02 +0330

    fix sidebar

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -20,90 +20,90 @@
         </div>
         <div class="w-100  d-flex flex-column px-2 mt-10">
             <div 
-            class="d-flex px-2 py-1 rounded app_pointer " 
+            class="d-flex px-2 py-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'dashboard'}"
             @click="navigateTo('/dashboard')"
             >
                 <img src="icons/dashboard.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Dashboard</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer  " 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'tasks'}"
             @click="navigateTo('/tasks')"
             >
                 <img src="icons/task.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Tasks</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'employes'}"
             @click="navigateTo('/employes')"
             >
                <img src="icons/employes.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Employes</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center " 
             :class="{'app-bg-gray': Route.name === 'payment'}"
             @click="navigateTo('/payment')"
             >
                 <img src="icons/payment.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Payment</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'products'}"
             @click="navigateTo('/products')"
             >
              <img src="icons/product.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Products</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'wareHouse'}"
             @click="navigateTo('/wareHouse')"
             >
                 <img src="icons/wareHouse.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-2" 
                 v-if="isOpen"
                 >Ware House</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'permissions'}"
             @click="navigateTo('/permissions')"
             >
                 <img src="icons/premision.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-2" 
                 v-if="isOpen"
                 >Permissions</span>
             </div>
             <div 
-            class="d-flex px-2 py-1 mt-1 rounded app_pointer" 
+            class="d-flex px-2 py-1 mt-1 rounded app_pointer align-center" 
             :class="{'app-bg-gray': Route.name === 'setting'}"
             @click="navigateTo('/setting')"
             >
                <img src="icons/setting.svg">
                 <span 
-                class="app-font-size-12 app-font-weight-400 px-2 " 
+                class="app-font-size-12 app-font-weight-400 px-2 pt-1" 
                 v-if="isOpen"
                 >Setting</span>
             </div>
